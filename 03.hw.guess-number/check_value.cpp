4d3a2094621e4b16809bea93242e55a9ac2c68d9
Andrey Kovalenko <frolik0777@mail.ru>
2024-04-22 22:36:52 +0300

    Доп. задание 3: * хранение результатов в формате <username> score1 score2 .. scoreN * добавлен аргумент запуска -help * для "-table" печать только лучших результатов * добавлен аргумент запуска -tabel-full

--- 03.hw.guess-number/check_value.cpp
+++ 03.hw.guess-number/check_value.cpp
@@ -1,56 +1,145 @@
 #include <iostream>
 #include <ctime>
 #include <fstream>
+#include <limits>
 
 
 static const std::string high_scores_filename = "high_scores.txt";
 static int max_value = 100;
 
 
-// Write new high score to the records table
-int write_highscore(const std::string & user_name, const int attempts_count) {
-	// We should open the output file in the append mode - we don't want
-	// to erase previous results.
-	std::ofstream out_file{high_scores_filename, std::ios_base::app};
-	if (!out_file.is_open()) {
-		std::cout << "Failed to open file for write: " << high_scores_filename << "!" << std::endl;
+void print_help(std::ostream & file) {
+	file << "Usage:" << std::endl;
+	file << "\t -help           Show this message and exit" << std::endl;
+	file << "\t -max <value>    Set the max random value to guess, default is " << max_value << std::endl;
+	file << "\t -table          Show the high score table" << std::endl;
+	file << "\t -table-full     Show the full high score table" << std::endl;
+}
+
+int get_next_high_score(std::ifstream & in_file, int & high_score) {
+	if (in_file.fail()) {
 		return -1;
 	}
 
-	// Append new results to the table:
-	out_file << user_name << ' ';
-	out_file << attempts_count;
-	out_file << std::endl;
+	// ignore all spaces
+	char next_char = in_file.peek();
+	while (next_char == ' ')  {
+		in_file.ignore();
+		next_char = in_file.peek();
+	}
+
+	if (!std::isdigit(next_char)) {
+		return -1;
+	}
+
+	// Read the high score next
+	in_file >> high_score;
+	return 0;
+}
+
+// Write new high score to the records table
+// line format: <username> score1 score2 .. scoreN
+int write_highscore(const std::string & user_name, const int attempts_count) {
+
+	const std::string tmp_file = high_scores_filename + "_";
+	std::remove(tmp_file.c_str());
+
+	std::ifstream in_file{high_scores_filename, std::ios_base::in};
+	if (!in_file.is_open()) {
+		std::cout << "Failed to open file: " << high_scores_filename << "!" << std::endl;
+		return -1;
+	}
+	std::ofstream out_file{tmp_file, std::ios_base::app};
+	if (!out_file.is_open()) {
+		std::cout << "Failed to open file: " << tmp_file << "!" << std::endl;
+		return -1;
+	}
+
+	bool added = false;
+	while (true) {
+		std::string user_name_r;
+		int high_score = 0;
+
+		// Read the username first
+		in_file >> user_name_r;
+		if (in_file.fail()){
+			break;
+		}
+
+		out_file << user_name_r;
+		while (get_next_high_score(in_file, high_score) == 0) {
+			out_file << ' ' << high_score;
+		}
+
+		if (user_name_r == user_name) {
+			// Append new result for the user
+			out_file << ' ' << attempts_count;
+			added = true;
+		}
+
+		out_file << std::endl;
+	}
+
+	if (!added) {
+		// Append new user and results to the table
+		out_file << user_name << ' ';
+		out_file << attempts_count;
+		out_file << std::endl;
+	}
+
+	std::remove(high_scores_filename.c_str());
+	std::rename(tmp_file.c_str(), high_scores_filename.c_str());
+
 	return 0;
 }
 
 
 // Read the high score file and print all results
-int print_highscore_table() {
+int print_highscore_table(const bool bests_only) {
 	std::ifstream in_file{high_scores_filename};
 	if (!in_file.is_open()) {
 		std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
 		return -1;
 	}
 
-	std::cout << std::endl << "High scores table:" << std::endl;
+	if (!bests_only) {
+		std::cout << "Full ";
+	}
+	std::cout << "High scores table:" << std::endl;
 
 	std::string username;
-	int high_score = 0;
 	while (true) {
 		// Read the username first
 		in_file >> username;
-		// Read the high score next
-		in_file >> high_score;
-		// Ignore the end of line symbol
-		in_file.ignore();
-
 		if (in_file.fail()) {
 			break;
 		}
 
-		// Print the information to the screen
-		std::cout << username << '\t' << high_score << std::endl;
+		std::cout << username;
+
+		if (bests_only) {
+			// print best result only
+			int hscore_best = std::numeric_limits<int>::max();
+			int high_score = hscore_best;
+			bool best_found = false;
+			while (get_next_high_score(in_file, high_score) == 0) {
+				hscore_best = std::min(high_score, hscore_best);
+				best_found = true;
+			}
+			if (best_found) {
+				std::cout << '\t' << hscore_best;
+			} else {
+				std::cout << "\t <no data>";
+			}
+		}
+		else {
+			// print all results
+			int high_score = 0;
+			while (get_next_high_score(in_file, high_score) == 0) {
+				std::cout << '\t' << high_score;
+			}
+		}
+		std::cout << std::endl;
 	}
 	return 0;
 }
@@ -62,7 +151,11 @@ void parse_arguments(int argc, char** argv) {
 		const std::string arg_value{ argv[0] };
 		--argc;
 		++argv;
-		if (arg_value == "-max") {
+		if (arg_value == "-help") {
+			print_help(std::cout);
+			std::exit(EXIT_SUCCESS);
+		}
+		else if (arg_value == "-max") {
 			if (argc == 0) {
 				std::cerr << "Missing parameter for argument \"" << arg_value << "\"" << std::endl;
 				std::exit(EXIT_FAILURE);
@@ -72,11 +165,16 @@ void parse_arguments(int argc, char** argv) {
 			++argv;
 		}
 		else if (arg_value == "-table") {
-			print_highscore_table();
+			print_highscore_table(true);
+			std::exit(EXIT_SUCCESS);
+		}
+		else if (arg_value == "-full-table") {
+			print_highscore_table(false);
 			std::exit(EXIT_SUCCESS);
 		}
 		else {
 			std::cerr << "Unknown argument \"" << arg_value << "\"" << std::endl;
+			print_help(std::cerr);
 			std::exit(EXIT_FAILURE);
 		}
 	}
@@ -99,9 +197,9 @@ int main(int argc, char** argv) {
 	// print the debug inforamation
 	std::cout << "[DEBUG] target_value = " << target_value << std::endl;
 	std::cout << "[DEBUG] max_value = " << max_value << std::endl;
-
 	int attempts_count = 0;
 	int current_value = 0;
+
 	bool not_win = true;
 
 	std::cout << "Enter your guess:" << std::endl;
@@ -127,7 +225,7 @@ int main(int argc, char** argv) {
 		std::cerr << "Unable to store results to the file " << high_scores_filename << std::endl;
 		return EXIT_FAILURE;;
 	}
-	if (print_highscore_table() != 0) {
+	if (print_highscore_table(true) != 0) {
 		std::cerr << "Unable to read results frome the file " << high_scores_filename << std::endl;
 		return EXIT_FAILURE;;
 	}
